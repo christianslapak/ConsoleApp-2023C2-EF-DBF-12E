c65c1968d5b05593551086c30f67c491877dfdc2
christian.slapak <christian.slapak@ort.edu.ar>
2023-10-11 20:23:37 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp-2023C2-EF-DBF-12E/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp_2023C2_EF_DBF_12E.Modelo;
+using System;
+using System.Linq;
+
+namespace ConsoleApp_2023C2_EF_DBF_12E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new PruebaEF2023C2Context())
+            {
+                var consulta = context.Student.Where(s => s.StudentId == 2);
+                var estudiante = consulta.FirstOrDefault<Student>();
+                Console.WriteLine("Encontré al estudiante 2 y se llama " + estudiante.StudentName);
+                Console.ReadKey();
+
+                var std = new Student()
+                {
+                    StudentName = "Alejandro"
+                };
+                context.Student.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+}
